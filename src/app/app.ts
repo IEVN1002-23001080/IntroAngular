806c43377f6b70ffe0a4624ac71d7f2a26295154
Jassiel <85763@alumnos.utleon.edu.mx>
2026-09-11 20:47:38 -0600

    Heroes-HTML 11-sep-26

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,23 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
 })
 export class App {
+
   protected readonly title = signal('IntroAngular');
+  titulo: string = "Jassiel Vazquez";
+  
+  // Agregamos la propiedad suma para que Angular la reconozca en el HTML
+  suma: number = 2 + 2;
+
+  duplicaNumero(num: number): number {
+    return num * 2;
+  }
+
+  pelicula = {
+    titulo: "El padrino",
+    anio: 1972,
+    genero: "Crimen/Drama",
+    fechaLanzamiento: new Date(),
+    precio: 345
+  };
+  
 }
